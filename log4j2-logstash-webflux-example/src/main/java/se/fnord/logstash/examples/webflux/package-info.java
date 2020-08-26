8948952833b94314283bb92900d44f4c26b2bb1f
Henrik Gustafsson <henrik.gustafsson@fnord.se>
2020-08-26 13:46:38 +0100

    Add Logstash-style Webflux server request logging example

---
+++ log4j2-logstash-webflux-example/src/main/java/se/fnord/logstash/examples/webflux/package-info.java
@@ -0,0 +1,5 @@
+@DefaultAnnotation(NonNull.class)
+package se.fnord.logstash.examples.webflux;
+
+import edu.umd.cs.findbugs.annotations.DefaultAnnotation;
+import edu.umd.cs.findbugs.annotations.NonNull;

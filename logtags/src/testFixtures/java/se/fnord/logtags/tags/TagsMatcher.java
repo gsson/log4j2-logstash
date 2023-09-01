4b1d2483bcb4de8d12c9ad381d7d63105ed95910
Henrik Gustafsson <henrik.gustafsson@fnord.se>
2023-09-01 13:47:10 +0100

    Add TagsMatcher class for testing tag equality

---
+++ logtags/src/testFixtures/java/se/fnord/logtags/tags/TagsMatcher.java
@@ -0,0 +1,36 @@
+package se.fnord.logtags.tags;
+
+import org.mockito.ArgumentMatcher;
+import org.mockito.Mockito;
+
+import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
+
+public class TagsMatcher implements ArgumentMatcher<Tags> {
+  private final List<TagsUtil.Tag> expectedTags;
+
+  public TagsMatcher(List<TagsUtil.Tag> expectedTags) {
+    this.expectedTags = expectedTags;
+  }
+
+  @Override
+  public boolean matches(Tags argument) {
+    var actualTags = TagsUtil.collectTags(argument);
+    return expectedTags.equals(actualTags);
+  }
+
+  @Override public String toString() {
+    return expectedTags.stream()
+        .map(Objects::toString)
+        .collect(Collectors.joining(", ", "[", "]"));
+  }
+
+  public static TagsMatcher of(TagsUtil.Tag ... tags) {
+    return new TagsMatcher(List.of(tags));
+  }
+
+  public static Tags tagsEq(TagsUtil.Tag ... tags) {
+    return Mockito.argThat(of(tags));
+  }
+}
